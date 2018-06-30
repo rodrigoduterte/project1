b7b4453a43843ffc7dd93cc79f16c340830dcc0b
Phung Pham <phungpham.tech@gmail.com>
2018-06-30 10:56:54 -0400

    .3

--- assets/js/controls.js
+++ assets/js/controls.js
@@ -1,12 +1,11 @@
-//Trigger the modal 1 - Adding new customer
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
-  });
-
-  // Or with jQuery
 
   $(document).ready(function(){
-    $('.modal').modal();
+    //Initialize the modal 
+    $(".modal").modal();
+    //Open the modal adding new leads
+    $("#modal1").modal("open");
+    //
   });
+
+
        
